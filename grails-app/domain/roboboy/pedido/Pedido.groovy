0234ff4285cc16d556fe58b5257311474cd6c5ad
newton <newton@motorola.com>
2017-09-27 23:26:29 -0300

    javascript e front de fazer pedido funcionando

--- grails-app/domain/roboboy/pedido/Pedido.groovy
+++ grails-app/domain/roboboy/pedido/Pedido.groovy
@@ -1,12 +1,12 @@
 package roboboy
 import grails.rest.*;
 import roboboy.Usuario;
+import roboboy.pedido.Pizza
 
 class Pedido {
   Pizza pizza;
   static belongsTo = [ usuario: Usuario ]
 
   static constraints = {
-    sabor2 nullable: true
   }
 }
--- grails-app/domain/roboboy/pedido/Pizza.groovy
+++ grails-app/domain/roboboy/pedido/Pizza.groovy
@@ -3,5 +3,7 @@ package roboboy.pedido
 class Pizza {
 
     static constraints = {
+    	String sabor1;
+    	String sabor2;
     }
 }
